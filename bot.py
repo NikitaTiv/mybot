978b2678aba4ef5aceca93f1ed38aa369691a7b2
NikitaTiv <118524352+NikitaTiv@users.noreply.github.com>
2022-11-23 17:30:07 +0300

    Changed

--- bot.py
+++ bot.py
@@ -1,27 +1,34 @@
+# Импорт библиотек
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+# Импортирование файла с токеном и данными для входа
 import settings
 
+# Насторойка вывода логов
 logging.basicConfig(filename='bot.log', 
                     format='[%(asctime)s] [%(levelname)s] => %(message)s', 
                     level=logging.INFO)
 
+#Настройка прокси сервера
 PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 def greet_user(update, context):
+    '''Функция вызываемая про вводе пользователем /start'''
     print('Вызван /start')
     update.message.reply_text('Здравствуй пользователь!')
 
 
 def talk_to_me(update,context):
+    '''Функция вызываемая при вводе пользователем текстового сообщения'''
     text = update.message.text
     print(text)
     update.message.reply_text(text)
 
 
 def main():
+    '''Создание тела бота, настройка диспетчера'''
     mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
@@ -32,5 +39,6 @@ def main():
     mybot.start_polling()
     mybot.idle()
 
+#Прописываем if чтобы при импорте в другую программу, не запустился бот и все не сломалось
 if __name__ == '__main__':
     main()
